776d5396d00765541e7ea409019d87cceb7f7842
Nam Nguyen <57205067+izdaezt@users.noreply.github.com>
2021-11-15 08:27:35 +0700

    innit project add function count

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <button @click="()=>{count++}">count is: {{ count }}</button>
+    <button @click="counted">count is: {{ count2 }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+
+export default class HelloWorld extends Vue {
+  public msg!: string
+  // Declared as component data
+  count = 0
+  count2 = 0
+
+  counted (): void {
+    this.count2 += 1
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
